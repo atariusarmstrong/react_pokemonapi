96512e96d3bd30217875810a0a0a6e1ab6fbfb82
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-04-15 15:30:38 -0400

    able to update state search upon input

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class Pokedex extends Component {
+    state = {
+        search: null,
+        pokemon: null
+    }
+
+    handleChange = (e) => {
+        this.setState({search: e.target.value})
+    }
+
+    // searchPokemon = (e) => {
+    //     e.preventDefault()
+    //     let newPokemon = axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.search}/`)
+    // }
+    // componentDidMount = () => {
+    //     this.searchPokemon()
+    // }
+
     render() {
         return (
             <div>
-                <form>
-                    <input placeholder="Pokemon name or number..."></input>
+                <form onSubmit={this.searchPokemon}>
+                    <input onChange={this.handleChange} placeholder="Pokemon name or number..."></input>
                     <button>Search</button>
                 </form>
                 <div>
-                    
+
                 </div>
             </div>
         );
