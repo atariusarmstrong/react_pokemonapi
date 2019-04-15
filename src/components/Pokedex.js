2b7e5559727ef2adfd9cd51e42498dd3df2e2d28
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-04-15 18:05:26 -0400

    retrieving pokemon data in console log upon search

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -11,18 +11,21 @@ class Pokedex extends Component {
         this.setState({search: e.target.value})
     }
 
-    // searchPokemon = (e) => {
-    //     e.preventDefault()
-    //     let newPokemon = axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.search}/`)
-    // }
-    // componentDidMount = () => {
-    //     this.searchPokemon()
-    // }
+    searchPokemon = async (e) => {
+        // e.preventDefault()
+        console.log(`searching ${this.state.search}`)
+        let newPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.search}/`)
+        this.setState({pokemon: newPokemon.data})
+        console.log(this.state.pokemon)
+    }
+    componentDidMount(){
+        this.searchPokemon()
+    }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.searchPokemon}>
+                <form onSubmit={e => {e.preventDefault(); this.searchPokemon()}}>
                     <input onChange={this.handleChange} placeholder="Pokemon name or number..."></input>
                     <button>Search</button>
                 </form>
