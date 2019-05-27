caad707765287d81c8a5d483d3ba364ade3dad5f
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-05-26 23:40:13 -0400

    styled header

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Pokemon React App</h1>
+        <h1 className="header justify-content-md-center">Pokemon React App</h1>
         <Trainer 
           trainer = {this.state.trainer}
           changeTrainerName = {this.changeTrainerName}
